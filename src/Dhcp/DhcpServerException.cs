f0e6d2eead00c7f34ca5e64674d46b41b9bda76b
Gary Sharp <garypsharp@hotmail.com>
2020-05-21 17:24:43 +1000

    multithreaded exception handling

--- src/Dhcp/DhcpServerException.cs
+++ src/Dhcp/DhcpServerException.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.ComponentModel;
 using System.Text;
 using Dhcp.Native;
@@ -8,7 +8,7 @@ namespace Dhcp
 {
     public class DhcpServerException : Exception
     {
-        private readonly static Dictionary<DhcpServerNativeErrors, string> errorDescriptions = new Dictionary<DhcpServerNativeErrors, string>();
+        private readonly static ConcurrentDictionary<DhcpServerNativeErrors, string> errorDescriptions = new ConcurrentDictionary<DhcpServerNativeErrors, string>();
         private readonly string message;
 
         public string ApiFunction { get; }
@@ -55,7 +55,7 @@ namespace Dhcp
             }
 
             // add to cache
-            errorDescriptions[nativeError] = description ??= "Unknown Error";
+            errorDescriptions.TryAdd(nativeError, description ??= "Unknown Error");
 
             return description;
         }
